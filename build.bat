3f2142bd1ac5a43aec35aac73394a35ac610150f
Ayibatari Ibaba <tariibabadev@gmail.com>
2023-01-14 13:23:06 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 md dist
-"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "src\win-e.ahk" /out "dist\win-e.exe"
+"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "src\win-e.ahk" /out "dist\Win-E-File-Explorer-Tab.exe"
